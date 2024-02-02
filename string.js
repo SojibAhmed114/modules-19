7cd43eab366652e3cc93edd1e29810c617fbfade
SojihAhmed114 <sojib.ahmed7620@gmail.com>
2024-02-02 19:49:46 +0600

    18.2 sesh

---
+++ string.js
@@ -0,0 +1,24 @@
+
+//object type;
+// let/const/var meanfulName = new string ("any value")
+// const thana = new String(`demra`);
+// console.log(typeof thana);
+// const upjala = new String("dhaka");
+// console.log(typeof upjala);
+
+// let nam = "sojib ahemd safi";
+// console.log(nam.length);
+// console.log(nam[3]);
+
+
+
+let mama = ["sojib", "safi", "ahemd", "mizanur"];
+mama[2] = "Name nai";
+console.log(mama);
+output: ['sojib', 'safi', 'Name nai', 'mizanur']
+ let names = "dhaka";
+names[0] = "F"
+console.log(names); //output: dhaka e thakbe.
+
+
+
---
+++ upperCase-LowerCase.js
@@ -0,0 +1,21 @@
+// string type upperCase and lowerCase ‍and trim. trim মানে হলো অপ্রয়োজনীয় জিনিস থাকলে ফেলো দেয়া । সেটা হতে পারে ফাকা স্পেস।
+// let caseOne = "i live in bangladesh, i have no idea about india and bangladesh";
+// let caseTwo = "THE UPPER CASE TRANSFORMER WILL TAKE ANY TEXT THAT YOU HAVE AND WILL GENERATE ALL THE LETTERS INTO UPPER CASE ONES.";
+// console.log(caseOne.toUpperCase(), caseTwo.toLowerCase());
+
+// // more example;
+// const subject = "Islamic History";
+// const book = "islamic history";
+// if (subject.toLowerCase() === book.toLowerCase()) {
+//     console.log("Mashaallah, nice");
+    
+// }
+// else {
+//     console.log("Very‘, nice ");
+// }
+
+//trim
+const money = "   nei";
+let face = "valoNa       ";
+console.log(money.trim());
+console.log(face.trim()); 
